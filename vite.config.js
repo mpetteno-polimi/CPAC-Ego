6108136725d44b851eb25fd121e34d78ac39c46c
Matteo Pettenò <matteo.petteno@mail.polimi.it>
2022-12-08 23:32:45 +0100

    [INIT] Svelte app \w Vite template - Svelte Kit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()]
+})
