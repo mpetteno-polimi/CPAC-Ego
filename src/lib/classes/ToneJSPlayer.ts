813f5f5929d156f48d25291db05d6ebda78ff266
gosammot <109701026+gosammot@users.noreply.github.com>
2023-02-20 12:06:25 -0800

    More work to the synths

--- src/lib/classes/ToneJSPlayer.ts
+++ src/lib/classes/ToneJSPlayer.ts
@@ -4,26 +4,26 @@ export default class ToneJSPlayer {
 
     synthChord: Tone.Synth<Tone.SynthOptions>;
     synthDrone: Tone.Synth<Tone.SynthOptions>;
-    synthNoiseDrone: Tone.Synth<Tone.SynthOptions>;
+    synthNoiseDrone: Tone.NoiseSynth;
     synthLead: Tone.Synth<Tone.SynthOptions>;
     synthBass: Tone.Synth<Tone.SynthOptions>;
     reverb: Tone.Reverb;
+    filterLead: Tone.Filter;
+    filterDrone: Tone.Filter;
 
     constructor() {
 
-        console.log("Initializing synths")
-
+        // instantiate the reverb
         this.reverb = new Tone.Reverb ({
             decay : 1.5 ,
-            preDelay : 0.01
+            preDelay : 0.01,
+            wet : 0.5
             });
-        // this.reverb.connect(Tone.Master);
-        this.synthChord = new Tone.Synth().toDestination();
-        this.synthDrone = new Tone.Synth().toDestination();
-        this.synthNoiseDrone = new Tone.Synth().toDestination();
+
+        // instantiate the lead synth
         this.synthLead = new Tone.Synth({
 
-            "portamento" : 0.02,
+            "portamento" : 0.08,
             "oscillator": {
                 "type": "square4"
             },
@@ -34,7 +34,13 @@ export default class ToneJSPlayer {
                 "release": 2
             },
             "volume" : - 12
-        }).toDestination();
+        });
+
+        // instantiate the low pass for the lead synth
+        this.filterLead = new Tone.Filter(4000, "lowpass");
+        this.filterLead.chain(this.filterLead,this.reverb,Tone.Destination);
+
+        // instantiate the bass
         this.synthBass = new Tone.Synth({
                 "oscillator": {
                     "type": "fmsquare5",
@@ -43,54 +49,116 @@ export default class ToneJSPlayer {
                       "harmonicity" : 0.501
                 },
 
+                "portamento" : 0.08,
                 "envelope": {
                     "attack": 0.01,
                     "decay": 0.1,
                     "sustain": 0.4,
                     "release": 2
-                }
+                },
+                "volume" : - 12
             }).toDestination();
+
+            /*
+            this.synthChord = new Tone.Synth().toDestination();
+            */
+
+            // instastiate the pitched drone
+            this.synthDrone = new Tone.Synth({
+
+                    "oscillator": {
+                        "type": "fatsine4",
+                        "spread" : 60,
+                        "count" : 10
+                    },
+                    "envelope": {
+                        "attack": 0.4,
+                        "decay": 0.01,
+                        "sustain": 1,
+                        "attackCurve" : "sine",
+                        "releaseCurve" : "sine",
+                        "release": 0.4
+                    }
+                }).toDestination();
+
+            this.filterDrone = new Tone.Filter(2000, "lowpass");
+            this.filterDrone.chain(this.filterDrone,Tone.Destination);
+            
+            // instantiate the noise drone
+            this.synthNoiseDrone = new Tone.NoiseSynth({
+                    "noise": {
+                    "type": "pink",
+                    "playbackRate" : 0.1
+                },
+                "envelope": {
+                    "attack": 0.5,
+                    "decay": 2,
+                    "sustain": 0.5,
+                    "release": 3
+                }
+            });
+
+            this.synthNoiseDrone.chain(this.reverb,Tone.Destination);
+        
+         // for debug
+         /*
+         this.synthDrone.triggerAttack("C2", Tone.now() + 1);
+         this.synthNoiseDrone.triggerAttack("C2", Tone.now() + 2);
+         */
+        
 
     }
 
     playChord(note) {
         let noteString = Tone.Frequency(note, "midi").toNote();
-        this.synthChord.triggerAttackRelease(noteString, "8n");
+        this.synthChord.triggerAttackRelease(noteString, "1n");
     }
 
+
     startDrone() {
-        console.log("Start drone")
         let now = Tone.now();
         this.synthDrone.triggerAttack("C2", now);
     }
 
     startNoiseDrone() {
-        console.log("Start noise drone")
         let now = Tone.now();
         this.synthNoiseDrone.triggerAttack("C2", now);
     }
 
     stopDrone() {
-        console.log("Stop drone")
         let now = Tone.now();
-        this.synthNoiseDrone.triggerRelease(now + 1);
+        this.synthNoiseDrone.triggerRelease(now);
     }
 
     playNote(note) {
-        console.log("play note")
-        console.log(note);
+        let now = Tone.now();
         let noteString = Tone.Frequency(note, "midi").toNote();
-        this.synthLead.triggerAttackRelease(noteString, "1n");
+        this.synthLead.triggerAttack(noteString, now);
 
     }
 
     playBass(note) {
+        let now = Tone.now();
         let noteString = Tone.Frequency(note, "midi").toNote();
-        this.synthBass.triggerAttackRelease(noteString, "2n");
+        this.synthBass.triggerAttack(noteString, now);
     }
 
-    setAudioParams(p1, p2, p3, p4) {
-        // todo
+
+    setAudioParams(p1, p2, p3) {
+
+        this.filterLead.set({
+            frequency: 2000 + p1 * 1000
+        });
+
+        this.filterDrone.set({
+            frequency: 500 + p2 * 1000
+        });
+
+        this.reverb.set({
+            wet: 0.6 * p3
+        });
+
+
     }
 
 }
